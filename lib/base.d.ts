ad85f752927da8c631803d5092ae900ad370da0d
Kunal Gosrani <iamkunalgosrani@gmail.com>
2020-10-23 07:15:50 +0530

    lib folder added to gitignore

--- lib/base.d.ts
+++
@@ -1,4 +0,0 @@
-export declare abstract class TypeBase<T> {
-    readonly _type: T;
-    abstract parse(value: unknown): Promise<T | null | undefined>;
-}
--- lib/errors.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type PathErrorObject = {
-    field: string;
-    message: string;
-};
-export declare class ParsingError extends Error {
-    path?: PathErrorObject[];
-    constructor(message: string, path?: PathErrorObject[]);
-}
